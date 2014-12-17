8d8acd690911654ebfafc2185f066846922cf96b
mzikton <mzikton@gmail.com>
2014-12-16 21:29:18 -0500

    Commit 1
    Create the basic template of the project
    
    Able to compute SHA1, and show the result to user

--- HashChecker/HashChecker/Form1.Designer.cs
+++ HashChecker/HashChecker/Form1.Designer.cs
@@ -28,12 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(72, 78);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 81);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "SHA1 Calculator";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(292, 271);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "HashChecker";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- HashChecker/HashChecker/Form1.cs
+++ HashChecker/HashChecker/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
 
 namespace HashChecker
 {
@@ -16,5 +18,55 @@ namespace HashChecker
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //Take a stream of data from a file
+            Stream file = chooseFile();
+
+            //If the file was readable, no errors occured
+            if (file != null)
+            {
+                //Initialize an instance of the SHA1 class calculator
+                HashAlgorithm sha = new SHA1CryptoServiceProvider();
+
+                //Compute the hash of the file and store it in an array of bytes
+                byte[] result = sha.ComputeHash(file);
+
+                //Make this array readable
+                string message = BitConverter.ToString(result).Replace("-", string.Empty);
+
+                //Show it
+                MessageBox.Show(message);
+            }
+
+        }
+
+        private Stream chooseFile()
+        {
+            Stream myStream = null;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = openFileDialog1.OpenFile()) != null)
+                    {
+                        return openFileDialog1.OpenFile();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(string.Format("{0}\n{1}", "Error, impossible to open the file. Error Code: ", ex.Message));
+                }
+            }
+            return null;
+        }
+
     }
 }
