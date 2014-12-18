fbeb24d43d7186071fc103bed0f35b6fa05bdd8e
mzikton <mzikton@gmail.com>
2014-12-17 21:03:17 -0500

    Licensied the code under MIT

--- HashChecker/HashChecker/Form1.cs
+++ HashChecker/HashChecker/Form1.cs
@@ -1,10 +1,35 @@
-﻿/*Programmer: aiglebleu at openmailbox dot org              Date: 2014-12-17
+﻿/*Programmer: aiglebleu at openmailbox dot org              Date: 2014-12-17    Developed under MIT License
  * 
  * --------------------------------------------
  * Declared Open-Sauce, you might do whatever
  * you want with these files. Crediting initial
  * author would be great thought.
  * --------------------------------------------
+ * Proper copyrights:
+ * 
+ * The MIT License (MIT)
+
+ * Copyright (c) <year> <copyright holders>
+
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * 
+ * 
  * Originally developed for: https://hightechlowlife.eu
 
          ,ggg,        gg  ,ggggggggggggggg       ,gggg,         ,gggg,        
